dc8d64b6c1a40eb0518ace6aa7ac031640966afd
nickaquilina <nickaquilina@gmail.com>
2021-04-01 19:41:51 +0200

    Data Variables Practical mostly ready
    I opted for the x,y notation since it is more natural. It is easier for me to take care data is loaded in the correct way once.

--- model1.py
+++ model1.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 """ Model:
@@ -9,48 +10,69 @@ import random
 """
 
 
-y0 = 50
-x0 = 50
-
-y1 = 50
-x1 = 50
-
-
-def gen_coordinates(y: int, x: int) -> ():
-    """ This function accepts two integers representing y,x coordinates, and
-        returns new coordinates, incremented or decremented by 1.
+def gen_coordinates(x, y):
+    """ This function accepts two integers representing y,x coordinates, and returns new
+        coordinates, incremented, or decremented by 1.
     """
     # Generate a random value between 0.0 and 1.0
-    y_rand = random.random()
     x_rand = random.random()
-    
-    print("y_rand: {}".format(y_rand))
-    print("x_rand: {}".format(x_rand))
+    y_rand = random.random()
 
-    if y_rand < 0.5:
-        y += 1
-    else:
-        y -= 1    
+    print("x_rand: {}".format(x_rand))
+    print("y_rand: {}".format(y_rand))
 
     if x_rand < 0.5:
         x += 1
     else:
-        x -= 1    
+        x -= 1
+
+    if y_rand < 0.5:
+        y += 1
+    else:
+        y -= 1
 
     return (x, y)
 
 
-print("(y0, x0): ({},{})".format(y0, x0))
-print("(y1, x1): ({},{})".format(y1, x1))
+def calc_euclidean_dist(x0, x1, y0, y1):
+    """ Function accepts 2 sets of coordinates and calculated the Euclidean
+        distance.
+    """
+    sqr_Dx = (x1-x0)**2
+    sqr_Dy = (y1-y0)**2
+
+    return math.sqrt(sqr_Dx + sqr_Dy)
+
+
+def gen_initial_coords(min_bound=0, max_bound=99):
+    """ Function to generate the random initial co-ordinates within a grid. The default
+        values are 0 & 99 for both x & y, however, these can be overridden.
+    """
+    x_rand = random.randint(min_bound, max_bound)
+    y_rand = random.randint(min_bound, max_bound)
+
+    return(x_rand, y_rand)
+
+
+# Generate the initial coordinates
+x0, y0 = gen_initial_coords()
+x1, y1 = gen_initial_coords()
+
+
+print(f"({x0}, {y0})")
+print(f"({x1}, {y1})")
 
 # Move the agents 2 times
+x0, y0 = gen_coordinates(x0, y0)
+x0, y0 = gen_coordinates(x0, y0)
 
-y0, x0 = gen_coordinates(y0, x0)
-y0, x0 = gen_coordinates(y0, x0)
+x1, y1 = gen_coordinates(x1, y1)
+x1, y1 = gen_coordinates(x1, y1)
 
-y1, x1 = gen_coordinates(y1, x1)
-y1, x1 = gen_coordinates(y1, x1)
-   
-print("(y0, x0): ({},{})".format(y0, x0))
-print("(y1, x1): ({},{})".format(y1, x1))
-    
+print(f"(x0, y0): ({x0},{y0})")
+print(f"(x1, y1): ({x1},{y1})")
+
+
+distance = calc_euclidean_dist(x0, x1, y0, y1)
+
+print(f"Distance: {distance}")
