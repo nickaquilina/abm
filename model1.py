d87bec7b6c2e338395121484093925c21739be98
nickaquilina <nickaquilina@gmail.com>
2021-04-01 12:54:43 +0200

    Now generating x,y coordinates in a single function, and moving the agents twice

--- model1.py
+++ model1.py
@@ -12,30 +12,45 @@ import random
 y0 = 50
 x0 = 50
 
-y1 = 40
-x1 = 40
+y1 = 50
+x1 = 50
 
 
-def generate_movement(val: int) -> int:
-    """ This function accepts a value (one of the coordinates) and returns
-        either an increment, or decrement by 1
+def gen_coordinates(y: int, x: int) -> ():
+    """ This function accepts two integers representing y,x coordinates, and
+        returns new coordinates, incremented or decremented by 1.
     """
     # Generate a random value between 0.0 and 1.0
-    r = random.random()
+    y_rand = random.random()
+    x_rand = random.random()
+    
+    print("y_rand: {}".format(y_rand))
+    print("x_rand: {}".format(x_rand))
 
-    if r < 0.5:
-        val += 1
+    if y_rand < 0.5:
+        y += 1
     else:
-        val -= 1    
+        y -= 1    
 
-    return val 
+    if x_rand < 0.5:
+        x += 1
+    else:
+        x -= 1    
+
+    return (x, y)
 
 
-for x in range(10):
-    y0 = generate_movement(y0)
-    x0 = generate_movement(x0)
-    y1 = generate_movement(y1)
-    x1 = generate_movement(x1)
-    print("(y0, x0): ({},{})".format(y0, x0))
-    print("(y1, x1): ({},{})".format(y1, x1))
+print("(y0, x0): ({},{})".format(y0, x0))
+print("(y1, x1): ({},{})".format(y1, x1))
+
+# Move the agents 2 times
+
+y0, x0 = gen_coordinates(y0, x0)
+y0, x0 = gen_coordinates(y0, x0)
+
+y1, x1 = gen_coordinates(y1, x1)
+y1, x1 = gen_coordinates(y1, x1)
+   
+print("(y0, x0): ({},{})".format(y0, x0))
+print("(y1, x1): ({},{})".format(y1, x1))
     
