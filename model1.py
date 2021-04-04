b8dbea45c5d39cf537795e1558da21b651e6b10d
nickaquilina <nickaquilina@gmail.com>
2021-04-04 16:52:48 +0200

    Misc Updates
    Moved the agent creation to a function. Added a function to calculate the distances across all agents, using itertools, and added some testing for the  distance calculations

--- model1.py
+++ model1.py
@@ -1,12 +1,17 @@
+### TO DO
+### TIMING
+### TESTING
+
 import math
 import random
 #import operator
+import itertools
 import matplotlib.pyplot as pyplot
 
 # -----------------------------------------------------------------------------
 # Debugging helper. With the global DEBUG set to False, a lot of debug printing
 # is avoided.
-DEBUG = True
+DEBUG = False
 
 def dbgprint(m):
     if DEBUG:
@@ -61,36 +66,85 @@ def move_agent(xy_coords, overflow=100):
     dbgprint(f"Return: {c}\n")
     return c
 
+# Tutor supplied
+def distance_between(agents_row_a, agents_row_b):
+    return (((agents_row_a[0] - agents_row_b[0])**2) + ((agents_row_a[1] - agents_row_b[1])**2))**0.5
 
-def calc_euclidean_dist(x0, x1, y0, y1):
-    """ Function accepts 2 sets of coordinates and calculated the Euclidean
-        distance.
+
+def calc_distance(a1:[], a2:[]) -> float:
+    """ Function accepts 2 sets of coordinates and calculates the Euclidean
+        distance. a1 & a2 each represent a list of [x, y] coordinates, and 
+        should be provided in the form [x, y]
     """
-    sqr_Dx = (x1-x0)**2
-    sqr_Dy = (y1-y0)**2
+    """ Note on testing
+        For testing, I used the code provided by the tutor, and ran an equality
+        operator between the tutor's code and mine. I also saved a number of test
+        values and the expected results.
+        test_data = [[[47, 58], [70, 95], 43.56604182158393],
+                     [[47, 58], [47, 58], 0.0],
+                     [[47, 58], [25, 20], 43.9089968002003],
+                     [[25, 20], [70, 95], 87.46427842267951],
+                     [[25, 20], [47, 58], 43.9089968002003],
+                     [[44, 42], [70, 95], 59.033888572581766],
+                     [[44, 42], [47, 58], 16.278820596099706],
+                     [[37, 19], [70, 95], 82.85529554590944],
+                     [[37, 19], [47, 58], 40.26164427839479],
+                     [[37, 19], [25, 20], 12.041594578792296]]
+    """
+    sqr_Dx = (a2[0] - a1[0])**2
+    sqr_Dy = (a2[1] - a1[1])**2
 
     return math.sqrt(sqr_Dx + sqr_Dy)
 
+def calc_distance_all(agents_list: []) -> []:
+    """ Calculate the Eclidean distance between all agents. This excludes same
+        agent calculations and is order agnostic.
+    """
+    results = []
+    for agent1, agent2 in itertools.combinations(agents_list, 2):
+        results.append([agent1, agent2, calc_distance(agent1, agent2)])
+
+    return results    
+    
+
+
+def create_agents(num = 10):
+    """ Function to create a number of 'agent' pairs of co-ordinates and return
+        them in the form of a list of lists. Default is set to 10 pairs.
+    """
+    l_agents = []
+    for x in range(num):
+        l_agents.append(gen_initial_coords())
+    
+    return l_agents
+
+
 
 # Define the number of agents, and iterations per agent
-num_agents = 10
-num_iterations = 2
+num_agents = 4
+num_iterations = 10
 
 # Generate a number of agents
-agents = []
-#for x in range(1, num_agents + 1):
-for x in range(num_agents):
-    agents.append(gen_initial_coords())
-print(agents)
+agents = create_agents(num_agents)
 
 
 
-pyplot.ylim(0, 99)
-pyplot.xlim(0, 99)
+    
+    
+    
+
+
+
+
+"""
+
     
 for agent in agents:
     pyplot.scatter(agent[0], agent[1], marker=".")
 pyplot.show()
+
+
+
 
 
 for x in range(num_iterations):
@@ -101,6 +155,11 @@ for x in range(num_iterations):
         tmp.append(agent)
     agents = tmp
 
+
+    pyplot.ylim(0, 100)
+    pyplot.xlim(0, 100)
+    
+    
     for a in agents:
         pyplot.scatter(a[0], a[1], marker=".")
     
@@ -111,7 +170,7 @@ for x in range(num_iterations):
 
 
 
-
+"""
 
 
 # Print the largest based on the y coordinate
