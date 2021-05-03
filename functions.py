d38e11823357a4c47516e2eccf4c10f8f5074aab
nickaquilina <nickaquilina@gmail.com>
2021-05-03 23:30:17 +0200

    Model finished mostly

---
+++ functions.py
@@ -0,0 +1,182 @@
+""" This file contains all the functions used by the model script. Moved here for cleaner
+    main file.
+"""
+import os
+import sys
+import csv
+import time
+import shutil
+import logging
+import argparse
+import traceback
+import logging.handlers
+
+import agentframework
+
+
+def getLogger(name, cwd, debugToConsole=False):
+    """ A basic logging function. Since the script generates a lot of debug information,
+        rather than using the inefficient print function, I implemented simple logging,
+        where a logging object is acquired at the beginning and used throughout.
+
+        Info produces only minimal logging just to ensure all is ok, while the debug
+        writes copious amounts of data to a different log file.
+
+        Writing to the console is also parametrised, in that, by default, only info is
+        sent to the console (as well) Debug to console should only be used with smallish
+        runs, since it negatively affects performance.
+    """
+    logsToKeep = 5
+    logBytes = 5242880 # 5Mb * 1048576 bytes
+
+    debugFile = os.path.join(cwd, f"{name}.debug.log")
+    infoFile = os.path.join(cwd, f"{name}.info.log")
+
+    # Instead of deleting old log files, I am renaming uwing a timestamp. This ensures old
+    # log files are stored for posterity, while at the same making log viewing easier
+    # since only data for the current run is present.
+    listing = os.listdir(cwd)
+    for item in listing:
+        if ("debug.log" in item or "info.log" in item) and "backup" not in item:
+            try:
+                sourcePath = os.path.join(cwd, item)
+                newName = f"{int(time.time())}_backup_{item}"
+                destPath = os.path.join(cwd, "debuglogsarchive", newName)
+                shutil.move(sourcePath, destPath)
+            except PermissionError:
+                print(f"Cannot delete {sourcePath}")
+                print(traceback.format_exc())
+
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    msgFormat = "%(asctime)s %(levelname)-8s : %(module)-10s Ln:%(lineno)4d : %(message)s"
+    formatter = logging.Formatter(msgFormat)
+
+    # Console Handler
+
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(formatter)
+    if debugToConsole:
+        consoleHandler.setLevel(logging.DEBUG)
+    else:
+        consoleHandler.setLevel(logging.INFO)
+    logger.addHandler(consoleHandler)
+
+    debugHandler = logging.handlers.RotatingFileHandler(debugFile, maxBytes=logBytes,\
+                                                        backupCount = logsToKeep)
+
+    debugHandler.setFormatter(formatter)
+    debugHandler.setLevel(logging.DEBUG)
+    logger.addHandler(debugHandler)
+
+    infoHandler = logging.handlers.RotatingFileHandler(infoFile, maxBytes=logBytes, \
+                                                       backupCount = logsToKeep)
+    infoHandler.setFormatter(formatter)
+    infoHandler.setLevel(logging.INFO)
+    logger.addHandler(infoHandler)
+
+    return logger
+
+
+
+def parseArguments():
+    print(f"ARGV Length: {len(sys.argv)}")
+    if len(sys.argv) > 10:
+        # Using print here, since the logger is not yet avaialble
+        print("Too many arguments. Run the program with -h for help")
+        print("Exiting")
+        sys.exit()
+
+    parser = argparse.ArgumentParser(description="Agent Simulator. Random agents \
+                                                  interacting with the environment and \
+                                                  each other.")
+
+    parser.add_argument("-a", dest="numAgents", default=20,
+                        help='The number of Agents to spawn. Default is 10.')
+
+    parser.add_argument("-s", dest="stepIncr", default=0,
+                        help='Increments the Agents by this value on each run.')
+
+    parser.add_argument("-i", dest="numIterations", default=500,
+                        help="Iterations to run through. Default is 100")
+
+    parser.add_argument("-n", dest="neighbourhood", default=20,
+                        help="Neighbourhood distance. Default is 20")
+
+    parser.add_argument("-v", dest="verbose", action="store_true",
+                        help="Use this switch to enable debug logging to the console.")
+
+    args = parser.parse_args()
+
+    try:
+        numAgents = int(args.numAgents)
+        stepIncr = int(args.stepIncr)
+        numIterations = int(args.numIterations)
+        neighbourhood = int(args.neighbourhood)
+    except ValueError:
+        print("Values need to be integers")
+        print("Proceeding with the default variables")
+        print(traceback.format_exc())
+
+        defaults = {}
+        for key in vars(args):
+            defaults[key] = parser.get_default(key)
+
+        numAgents = defaults["numAgents"]
+        stepIncr = defaults["stepIncr"]
+        numIterations = defaults["numIterations"]
+        neighbourhood = defaults["neighbourhood"]
+    finally:
+        verbose = args.verbose
+        return numAgents, stepIncr, numIterations, neighbourhood, verbose
+
+
+def createAgents(env, xL, yL, amount, aList):
+    for i in range(amount):
+        aList.append(agentframework.Agent(env, xL, yL, aList))
+
+
+def loadEnvFromCSV(f):
+    """ Load the raster data from the Comma Seperated Values file
+    """
+    environment = []
+
+    try:
+        with open(f, "r") as openFile:
+            dataReader = csv.reader(openFile)
+            for row in dataReader:
+                rowlist = []
+                for col in row:
+                    rowlist.append(int(col))
+                environment.append(rowlist)
+
+    except FileNotFoundError:
+        print(f"File not found: {f}")
+        print(traceback.format_exc())
+        environment = None
+
+    return environment
+
+
+def saveEnvToCSV(data, csvFile):
+    """ Function to write CSV data back to a CSV file. No need to delete the
+        file prior to writing, since I amopening in write mode not append.
+    """
+    with open(csvFile, "wt", newline="") as openFile:
+        csvWriter = csv.writer(openFile)
+        for item in data:
+            csvWriter.writerow(item)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- model1.py
+++ model1.py
@@ -1,180 +1,27 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot
+
 # Builtin Imports
 import os
 import sys
-import csv
-import time
 import random
-import argparse
-import shutil
-import logging
-import logging.handlers
-import traceback
-# Other Imports
-import matplotlib.pyplot
+
 # Custom Imports
-import agentframework
+import functions
 
 
-# Retrieve the Current Working Directory and prepare the configurables
+# Retrieve the Current Working Directory and prepare the hard coded stuff
 # ----------------------------------------------------------------------------------------
 cwd = os.path.dirname(os.path.abspath(sys.argv[0]))
 name      = "Agent-Simulator"
 inFile    = "in.txt"
 outFile   = "out.txt"
 storeFile = "store.txt"
-
-
-def getLogger(name, cwd, debugToConsole=False):
-    """ A basic logging function. Since the script generates a lot of debug information,
-        rather than using the inefficient print function, I implemented simple logging,
-        where a logging object is acquired at the beginning and used throughout.
-
-        Info produces only minimal logging just to ensure all is ok, while the debug
-        writes copious amounts of data to a different log file.
-
-        Writing to the console is also parametrised, in that, by default, only info is
-        sent to the console (as well) Debug to console should only be used with smallish
-        runs, since it negatively affects performance.
-    """
-    logsToKeep = 5
-    logBytes = 5242880 # 5Mb * 1048576 bytes
-
-    debugFile = os.path.join(cwd, f"{name}.debug.log")
-    infoFile = os.path.join(cwd, f"{name}.info.log")
-
-    # Instead of deleting old log files, I am renaming uwing a timestamp. This ensures old
-    # log files are stored for posterity, while at the same making log viewing easier
-    # since only data for the current run is present.
-    listing = os.listdir(cwd)
-    for item in listing:
-        if ("debug.log" in item or "info.log" in item) and "backup" not in item:
-            try:
-                sourcePath = os.path.join(cwd, item)
-                newName = f"{int(time.time())}_backup_{item}"
-                destPath = os.path.join(cwd, "debuglogsarchive", newName)
-                shutil.move(sourcePath, destPath)
-            except PermissionError:
-                print(f"Cannot delete {sourcePath}")
-                print(traceback.format_exc())
-
-
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-    msgFormat = "%(asctime)s %(levelname)-8s : %(module)-10s Ln:%(lineno)4d : %(message)s"
-    formatter = logging.Formatter(msgFormat)
-
-    # Console Handler
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setFormatter(formatter)
-    if debugToConsole:
-        consoleHandler.setLevel(logging.DEBUG)
-    else:
-        consoleHandler.setLevel(logging.INFO)
-    logger.addHandler(consoleHandler)
-
-    debugHandler = logging.handlers.RotatingFileHandler(debugFile, maxBytes=logBytes,\
-                                                        backupCount = logsToKeep)
-
-    debugHandler.setFormatter(formatter)
-    debugHandler.setLevel(logging.DEBUG)
-    logger.addHandler(debugHandler)
-
-    infoHandler = logging.handlers.RotatingFileHandler(infoFile, maxBytes=logBytes, \
-                                                       backupCount = logsToKeep)
-    infoHandler.setFormatter(formatter)
-    infoHandler.setLevel(logging.INFO)
-    logger.addHandler(infoHandler)
-
-    return logger
-
-
-def parseArguments():
-    print(f"ARGV Length: {len(sys.argv)}")
-    if len(sys.argv) > 10:
-        # Using print here, since the logger is not yet avaialble
-        print("Too many arguments. Run the program with -h for help")
-        print("Exiting")
-        sys.exit()
-
-    parser = argparse.ArgumentParser(description="Agent Simulator. Random agents \
-                                                  interacting with the environment and \
-                                                  each other.")
-
-    parser.add_argument("-a", dest="numAgents", default=10,
-                        help='The number of Agents to spawn. Default is 10.')
-
-    parser.add_argument("-s", dest="agentStepIncr", default=0,
-                        help='Increments the Agents by this value on each run.')
-
-    parser.add_argument("-i", dest="numIterations", default=100,
-                        help="Iterations to run through. Default is 100")
-
-    parser.add_argument("-n", dest="neighbourhood", default=20,
-                        help="Neighbourhood distance. Default is 20")
-
-    parser.add_argument("-v", dest="verbose", action="store_true",
-                        help="Use this switch to enable debug logging to the console.")
-
-    args = parser.parse_args()
-
-    try:
-        numAgents = int(args.numAgents)
-        agentStepIncr = int(args.agentStepIncr)
-        numIterations = int(args.numIterations)
-        neighbourhood = int(args.neighbourhood)
-    except ValueError:
-        print("Values need to be integers")
-        print("Proceeding with the default variables")
-        print(traceback.format_exc())
-
-        defaults = {}
-        for key in vars(args):
-            defaults[key] = parser.get_default(key)
-
-        numAgents = defaults["numAgents"]
-        agentStepIncr = defaults["agentStepIncr"]
-        numIterations = defaults["numIterations"]
-        neighbourhood = defaults["neighbourhood"]
-    finally:
-        verbose = args.verbose
-        return numAgents, agentStepIncr, numIterations, neighbourhood, verbose
-
-
-
-def loadEnvFromCSV(f):
-    """ Load the raster data from the Comma Seperated Values file
-    """
-    environment = []
-
-    try:
-        with open(f, "r") as openFile:
-            dataReader = csv.reader(openFile)
-            for row in dataReader:
-                rowlist = []
-                for col in row:
-                    rowlist.append(int(col))
-                environment.append(rowlist)
-
-    except FileNotFoundError:
-        print(f"File not found: {f}")
-        print(traceback.format_exc())
-        environment = None
-
-    return environment
-
-
-def saveEnvToCSV(data, csvFile):
-    """ Function to write CSV data back to a CSV file. No need to delete the
-        file prior to writing, since I amopening in write mode not append.
-    """
-    with open(csvFile, "wt", newline="") as openFile:
-        csvWriter = csv.writer(openFile)
-        for item in data:
-            csvWriter.writerow(item)
-
+# ----------------------------------------------------------------------------------------
 
 # Parse the command line arguments
-numAgents, agentStepIncr, numIterations, neighbourhood, verbose = parseArguments()
+numAgents, stepIncr, numIterations, neighbourhood, verbose = functions.parseArguments()
 
 # Prepare the file names for IO
 inFile    = os.path.join(cwd, "in.txt")
@@ -182,92 +29,103 @@ outFile   = os.path.join(cwd, "out.txt")
 storeFile = os.path.join(cwd, "store.txt")
 
 # Get the logging object
-logger = getLogger(name, cwd, debugToConsole=verbose)
+logger = functions.getLogger(name, cwd, debugToConsole=verbose)
 logger.info(f"{name} initialised")
 logger.info("Commandline arguments parsed")
-
 logger.info(f"Name: {name}")
 logger.info(f"Agents: {numAgents}")
-logger.info(f"Agent Step Increment: {agentStepIncr}")
+logger.info(f"Agent Step Increment: {stepIncr}")
 logger.info(f"Iterations: {numIterations}")
 logger.info(f"Neighbourhood: {neighbourhood}")
 logger.info(f"Verbosity: {verbose}")
-
 logger.info(f"Environment File: {inFile}")
 logger.info(f"Environment Result: {outFile}")
 logger.info(f"Store File: {storeFile}")
 
-
 # Load the environment raster and discover the x & y limits
-environment = loadEnvFromCSV(inFile)
+environment = functions.loadEnvFromCSV(inFile)
 xLim = len(environment[0])
 yLim = len(environment)
 
+# Quick Configs for testing. Commented out when not needed
+#-----------------------------------------------------------------------------------------
+numAgents = 50
+stepIncr = 1
+numIterations = 0
+neighbourhood = 20
+verbose = True
+#-----------------------------------------------------------------------------------------
+
+#sys.exit()
 
 # Create the agents.
 agents = []
 logger.info("Creating agents.")
-for n in range(numAgents):
-    agents.append(agentframework.Agent(environment, xLim, yLim, agents))
+functions.createAgents(environment, xLim, yLim, numAgents, agents)
 logger.info(f"Done. There are {len(agents)} agents.")
-
-
 
 # Delete the store file if exists.
 if os.path.isfile(storeFile):
     os.remove(storeFile)
     logger.info(f"Store file {storeFile} deleted")
 
-# Move the agents.
-first = True
-for j in range(numIterations):
-    logger.info(f"Iteration: {j+1}")
-    """ Increment the number of agents if the agentStepIncr variable is greater than 0. A
-        A limit of 20 is hardcoded for now, since adding a lot of agents can lead to
-        performance problems
-    """
-    if (0 < agentStepIncr <= 20) and not first:
-        logger.info("Adding more agents.")
-        for n in range(agentStepIncr):
-            agents.append(agentframework.Agent(environment, xLim, yLim, agents))
-        logger.info(f"Done. There are {len(agents)} agents.")
-    first = False
 
-    #-------------------------------------------------------------------------------------
-    """
-    if j % int(numIterations/10) == 0:
-        logger.info(f"Iteration: {j}")
-    """
+fig, ax = matplotlib.pyplot.subplots()
+matplotlib.pyplot.xlim(0, xLim)
+matplotlib.pyplot.ylim(0, yLim)
+sc = ax.scatter(0, 0, marker=".", c="Black")
 
+# We only need to show the image once, then update the data. Otherwise, performance degrades
+image = matplotlib.pyplot.imshow(environment)
+
+def animate(i):
+    xy = []
     totalStore = 0
     random.shuffle(agents)
+    #Possibly update data only every x frames
+    image.set_data(environment)
+    for agent in agents:
+        agent.move()
+        agent.eat()
+        agent.shareWithNeighbours(neighbourhood)
+        totalStore += agent.store
+        xy.append([agent.x, agent.y])
 
-    for i in range(numAgents):
-        #print(f"Agent: {i} - id:{id(agents[i])}")
-        agents[i].move()
-        agents[i].eat()
-        agents[i].shareWithNeighbours(neighbourhood)
+    # Add more agents
+    # --------------------------------------------------------------------------------
+    if 0 < stepIncr <= 20:
+        #logger.info(f"Adding {stepIncr} more agents.")
+        functions.createAgents(environment, xLim, yLim, stepIncr, agents)
+        #logger.info("Done.")
+        print(len(agents))
 
-        totalStore += agents[i].store
 
     with open(storeFile, "at") as oStore:
         oStore.write(f"{totalStore}\n")
 
 
+    sc.set_offsets(xy)
 
-logger.info(f"Done. {numIterations} iterations executed.")
 
-
-matplotlib.pyplot.xlim(0, xLim)
-matplotlib.pyplot.ylim(0, yLim)
-
-matplotlib.pyplot.imshow(environment)
-for i in range(len(agents)):
-    matplotlib.pyplot.scatter(agents[i].x,agents[i].y, marker=".")
+ani = matplotlib.animation.FuncAnimation(fig, animate, frames=50, interval=1, repeat=True)
 matplotlib.pyplot.show()
 
 
-saveEnvToCSV(environment, outFile)
+
+functions.saveEnvToCSV(environment, outFile)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
